1718002c6efd2c2de8782fef9492d66efd6b1d0b
Abiti <nazrawimedhin1@gmail.com>
2022-04-09 10:35:33 +0300

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,28 @@
-#!/usr/bin/python3
-"""
-All files in the folder web_static must be added to the final archive
-All archives must be stored in the folder versions
-(your function should create this folder if it doesn't exist)
-The name of the archive created must be
-web_static_<year><month><day><hour><minute><second>.tgz
-The function do_pack must return the archive path
-if the archive has been correctly generated
-Otherwise, it should return None
-"""
-from fabric.api import *
-import time
-from datetime import date
+#!/usr/bin/env bash
+#
+# Set up Holberton web servers for the deployment of web_static
 
+apt update
+apt -y install nginx
 
-def do_pack():
-    timestamp = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(timestamp))
-        return ("versions/web_static_{:s}.tgz".format(timestamp))
-    except:
-        return None
+ufw allow 'Nginx HTTP'
+
+mkdir -p /data/web_static/shared /data/web_static/releases/test
+
+cat > /data/web_static/releases/test/index.html << 'EOF'
+<html>
+  <head>
+  </head>
+  <body>
+    Hello World
+  </body>
+</html>
+EOF
+
+ln -fns /data/web_static/releases/test /data/web_static/current
+
+chown -hR ubuntu:ubuntu /data
+
+sed -i '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+
+service nginx restart
