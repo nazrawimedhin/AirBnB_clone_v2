2a0dc7c95d2f2cee92a8dd956495ef5ec3385e5e
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2022-06-17 01:00:55 +0300

    correction

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Deletes out of date archives."""
+# Fabfile to delete out-of-date archives.
 import os
 from fabric.api import *
 
-
-env.hosts = ["34.148.9.175", "3.239.86.235"]
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_clean(number=0):
@@ -12,6 +11,10 @@ def do_clean(number=0):
 
     Args:
         number (int): The number of archives to keep.
+
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
     number = 1 if int(number) == 0 else int(number)
 
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,17 +2,26 @@
 
 # Nginx configuration file
 $nginx_conf = "server {
-	listen 80;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	add_header X-Served-By $hostname;
-	location /hbnb_static {
-		alias /data/web_static/current;
-		index 103-index.html;
-	}
-	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-	error_page 404 /custom_404.html;
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 }"
 
 package { 'nginx':
@@ -42,7 +51,7 @@ file { '/data/web_static/shared':
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
+  content => "Holberton School Puppet\n"
 } ->
 
 file { '/data/web_static/current':
@@ -64,7 +73,7 @@ file { '/var/www/html':
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Hello World!"
+  content => "Holberton School Nginx\n"
 } ->
 
 file { '/var/www/html/404.html':
